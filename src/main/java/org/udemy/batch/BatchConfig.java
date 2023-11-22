b22be644abe216054b4e19dfe42606785f735f5c
flavio.santana <9090flavio@gmail.com>
2023-11-22 12:25:12 -0300

    feat: Implementação do primeiro projeto Spring Batch

---
+++ src/main/java/org/udemy/batch/BatchConfig.java
@@ -0,0 +1,41 @@
+package org.udemy.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job imprimeOlaJob(){
+        return jobBuilderFactory
+                .get("imprimeOlaJob")
+                .start(imprimeOlaStep())
+                .build();
+
+    }
+
+    private Step imprimeOlaStep() {
+        return stepBuilderFactory
+                .get("imprimeOlaStep")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("Olá mundo Spring Batch!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+}
---
+++ src/test/java/org/udemy/batch/IniciandoSpringBatchMavenApplicationTests.java
@@ -0,0 +1,14 @@
+package org.udemy.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniciandoSpringBatchMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//TODO: Implementar testes
+	}
+
+}
