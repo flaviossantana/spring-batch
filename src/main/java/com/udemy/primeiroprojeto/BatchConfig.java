d4f268bdad241abf2b8c479a7ee3a0228d4f7cd7
flavio.santana <9090flavio@gmail.com>
2024-06-21 18:51:36 -0300

    feat: usando jobParameters na execução do job

--- src/main/java/com/udemy/primeiroprojeto/BatchConfig.java
+++ src/main/java/com/udemy/primeiroprojeto/BatchConfig.java
@@ -5,8 +5,12 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,17 +29,24 @@ public class BatchConfig {
         return jobBuilderFactory
                 .get("imprimiOlaJob")
                 .start(imprimeOlaMundoStep())
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    public Step imprimeOlaMundoStep() {
+        return stepBuilderFactory
+                .get("imprimeOlaMundoStep")
+                .tasklet(imprimeOlaTasklet(null))
                 .build();
     }
 
     @Bean
-    public Step imprimeOlaMundoStep() {
-        return stepBuilderFactory
-                .get("imprimeOlaMundoStep")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.printf("Olá mundo!");
-                    return RepeatStatus.FINISHED;})
-                .build();
+    @StepScope
+    public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome-parametro']}") String nome) {
+        return (contribution, chunkContext) -> {
+            System.out.println("###### Olá: " + nome + " ######");
+            return RepeatStatus.FINISHED;
+        };
     }
 
 }
