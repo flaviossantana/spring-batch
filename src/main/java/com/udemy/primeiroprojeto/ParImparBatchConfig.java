252a63dd32cd9c1b8c048199e41af752921c5c47
flavio.santana <9090flavio@gmail.com>
2024-06-21 19:13:03 -0300

    feat: usando chunk para processamento do JOB

---
+++ src/main/java/com/udemy/primeiroprojeto/ParImparBatchConfig.java
@@ -0,0 +1,61 @@
+package com.udemy.primeiroprojeto;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableBatchProcessing
+public class ParImparBatchConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job imprimeParOuImparJob() {
+        return jobBuilderFactory
+                .get("imprimeParOuImparJob")
+                .start(imprimeParOuImparStep())
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    public Step imprimeParOuImparStep() {
+        return stepBuilderFactory
+                .get("imprimeParOuImparStep")
+                .<Integer, String>chunk(1)
+                .reader(contaAteDezReader())
+                .processor(parOuImparProcessor())
+                .writer(imprimeParOuImparWriter())
+                .build();
+    }
+
+    private IteratorItemReader<Integer> contaAteDezReader() {
+        return new IteratorItemReader<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    }
+
+    private FunctionItemProcessor<Integer, String> parOuImparProcessor() {
+        return new FunctionItemProcessor<>(
+                numero -> String.format("Numero %s é %s: ", numero, numero % 2 == 0 ? "PAR" : "IMPAR")
+        );
+    }
+
+    private ItemWriter<String> imprimeParOuImparWriter() {
+        return numeros -> numeros.forEach(System.out::println);
+    }
+
+}
--- src/main/java/com/udemy/primeiroprojeto/ProjetoSpringBatchApplication.java
+++ src/main/java/com/udemy/primeiroprojeto/ProjetoSpringBatchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrimeiroProjetoApplication {
+public class ProjetoSpringBatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PrimeiroProjetoApplication.class, args);
+        SpringApplication.run(ProjetoSpringBatchApplication.class, args);
     }
 
 }
