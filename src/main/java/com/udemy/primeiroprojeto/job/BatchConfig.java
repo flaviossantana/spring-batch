140dbf434a711edb9c4ee7555ba53daa1e910cc4
flavio.santana <9090flavio@gmail.com>
2024-06-24 18:39:36 -0300

    feat: refatorando código

--- src/main/java/com/udemy/primeiroprojeto/job/BatchConfig.java
+++ src/main/java/com/udemy/primeiroprojeto/job/BatchConfig.java
@@ -1,11 +1,9 @@
-package com.udemy.primeiroprojeto;
+package com.udemy.primeiroprojeto.job;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,25 +15,13 @@ public class BatchConfig {
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
 
-    @Autowired
-    private StepBuilderFactory stepBuilderFactory;
-
     @Bean
-    public Job imprimiOlaJob() {
+    public Job imprimiOlaJob(
+            Step imprimeOlaMundoStep
+    ) {
         return jobBuilderFactory
                 .get("imprimiOlaJob")
-                .start(imprimeOlaMundoStep())
+                .start(imprimeOlaMundoStep)
                 .build();
     }
-
-    @Bean
-    public Step imprimeOlaMundoStep() {
-        return stepBuilderFactory
-                .get("imprimeOlaMundoStep")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.printf("Olá mundo!");
-                    return RepeatStatus.FINISHED;})
-                .build();
-    }
-
 }
